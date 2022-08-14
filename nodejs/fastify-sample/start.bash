2b6927d22a03cb15b9ee1216c9cd5527d8a34e85
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2022-08-15 00:24:34 +0900

    save some intial work, but not working

--- nodejs/fastify-sample/start.bash
+++ nodejs/fastify-sample/start.bash
@@ -2,26 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# https://nodejs.org/api/process.html#signal-events
-# https://linuxconfig.org/how-to-propagate-a-signal-to-child-processes-from-a-bash-script
-cleanup() {
-  >&2 echo ":: cleaning up..."
-}
-
 # TODO: needs to be refactored
 trap '>&2 echo ":: SIGINT:${child_pid}"; kill "${child_pid}"; wait "${child_pid}"; cleanup' SIGINT
 trap '>&2 echo ":: SIGTERM:${child_pid}"; kill "${child_pid}"; wait "${child_pid}"; cleanup' SIGTERM
 
-caddy start
-
+cd /app
 if [[ -z "${DEBUG:-}" ]]; then
 	>&2 echo ":: production"
-	node src/index.js &
+	node src/index.js
 else
 	>&2 echo ":: debug"
 	npm install nodemon
-	./node_modules/.bin/nodemon --config nodemon.json &
+	./node_modules/.bin/nodemon --config nodemon.json
 fi
-
-child_pid="$!"
-wait
