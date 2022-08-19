f50d77a8a41981bd9d0aa145330966c5ff58cf0b
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2022-08-19 19:41:42 +0900

    save work up to now

--- nodejs/fastify-sample/start.bash
+++ nodejs/fastify-sample/start.bash
@@ -2,11 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# TODO: needs to be refactored
-trap '>&2 echo ":: SIGINT:${child_pid}"; kill "${child_pid}"; wait "${child_pid}"; cleanup' SIGINT
-trap '>&2 echo ":: SIGTERM:${child_pid}"; kill "${child_pid}"; wait "${child_pid}"; cleanup' SIGTERM
-
-cd /app
+cd "${CWD}"
 if [[ -z "${DEBUG:-}" ]]; then
 	>&2 echo ":: production"
 	node src/index.js
