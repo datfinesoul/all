9120f1db1f5f78fe11990d6b7c7baf480cec4439
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2022-11-14 15:09:59 +0900

    pull review comments

---
+++ github/my-comments/pull-my-comments.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# vi: set noet :
+set -euo pipefail
+IFS=$'\n\t'
+
+if [[ ! -f issues.txt ]]; then
+  gh \
+    -R glg/devops-meetings issue list \
+    --state all \
+    --limit 300 \
+    --label Standup \
+    --search 2022 \
+    > issues.txt
+fi
+
+cat issues.txt \
+  | head -n200 \
+  | awk '{print $1}' \
+  | while read -r ISSUE_ID; do
+  echo "- $ISSUE_ID"
+  gh -R glg/devops-meetings issue view "${ISSUE_ID}" \
+    --comments \
+    --json comments \
+    --jq '.comments[] | select(.author.login=="datfinesoul")' \
+    >> datfinesoul.json
+done
+
+<datfinesoul.json jq -r '.body' > datfinesoul.md
+
